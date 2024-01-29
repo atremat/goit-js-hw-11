4335cd4946a1ba7ae1867ca11336876aad54d9cb
Artem <atremat@gmail.com>
2024-01-29 05:34:46 +0200

    gallery added

--- src/main.js
+++ src/main.js
@@ -1,9 +1,24 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 const refs = {
   form: document.querySelector('.form'),
   textInput: document.querySelector('#query'),
   searchBtn: document.querySelector('.search-button'),
   galleryList: document.querySelector('.gallery-list'),
   loadingStatus: document.querySelector('.loading'),
+};
+
+//options for simplelightbox
+const simplelightboxOptions = {
+  captions: true,
+  captionDelay: 250,
+  captionSelector: 'img',
+  captionType: 'attr',
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionClass: '',
+  captionHTML: true,
 };
 
 refs.form.addEventListener('submit', onSearch);
@@ -19,5 +34,50 @@ function onSearch(e) {
     orientation: 'horizontal',
     safesearch: true,
   });
-  console.log(`${searchParams}`);
+  refs.loadingStatus.classList.remove('is-hidden');
+  try {
+    fetch(`https://pixabay.com/api/?${searchParams}`)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error(response.status);
+        }
+        return response.json();
+      })
+      .then(data => {
+        console.log(data.hits);
+        refs.galleryList.innerHTML = createMarkup(data.hits);
+
+        //initializing simplelightbox
+        const gallery = new SimpleLightbox(
+          '.gallery-list a',
+          simplelightboxOptions
+        );
+      })
+      .catch(error => {
+        // Error handling
+        console.log(error);
+      });
+  } catch {
+    console.log('error');
+  }
+}
+
+function createMarkup(photoList) {
+  //creating markup
+  return photoList
+    .map(photo => {
+      return `
+      <li class="gallery-item">
+        <a class="gallery-link" href="${photo.largeImageURL}">
+          <img
+            class="gallery-image"
+            src="${photo.webformatURL}"
+            data-source="${photo.largeImageURL}"
+            alt="${photo.tags}"
+          />
+        </a>
+      </li>
+      `;
+    })
+    .join('\n');
 }
